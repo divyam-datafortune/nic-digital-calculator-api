8c015731c5d06825eccc3f7333734df5b62357f4
Divya Mahajan <divya.mahajan@neosofttech.com>
2020-07-14 19:50:16 +0530

    Done with upload pdf files on s3

--- services/aws.js
+++ services/aws.js
@@ -1,27 +1,25 @@
 const aws = require('aws-sdk');
-
 /**
  *To save pdf files on s3 bucket
  * @param pdf - request data
- * @returns {Promise<void>}
+ * @returns Pdf file  public url
  */
 module.exports.uploadPdfBuffer = async (buffer) => {
+
   aws.config.update({
     accessKeyId: process.env.CLOUDCUBE_ACCESS_KEY_ID,
     secretAccessKey: process.env.CLOUDCUBE_SECRET_ACCESS_KEY,
     region: process.env.CLOUDCUBE_REGION,
-    apiVersion: process.env.CLOUDCUBE_API_VERSION
   });
-
-  const s3 = new aws.S3();
-  const fileName = 'public/NICE-InContact-Recommendation-Report-' + Date.now() + '.pdf';
+  const fileName = `${process.env.MY_CLOUDCUBE}/public/NICE-InContact-Recommendation-Report-${Date.now()}.pdf`;
   const filePath = `${process.env.CLOUDCUBE_URL}/${fileName}`;
+
+  const s3           = new aws.S3({ apiVersion: 'latest' });
   const uploadParams = { Bucket: 'cloud-cube', Key: fileName, Body: buffer };
 
-  await s3.upload(uploadParams, function (err, data) {
+  s3.upload(uploadParams, function (err, data) {
     if (err) {
       console.log("Error", err);
-      return false
     }
   });
   return filePath;
