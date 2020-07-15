d2e6e6cf464a0b10257071da3daa3fc3279cbee5
Divya Mahajan <divya.mahajan@neosofttech.com>
2020-07-15 17:08:41 +0530

    Minor modification in function argument

--- services/aws.js
+++ services/aws.js
@@ -5,7 +5,7 @@ const aws = require('aws-sdk');
  * @param pdf - request data
  * @returns Pdf file  public url
  */
-module.exports.uploadPdfBuffer = async (buffer) => {
+module.exports.uploadPdfBuffer = async (pdfBuffer) => {
   const s3 = new aws.S3({
     accessKeyId: process.env.CLOUDCUBE_ACCESS_KEY_ID,
     secretAccessKey: process.env.CLOUDCUBE_SECRET_ACCESS_KEY,
@@ -14,7 +14,7 @@ module.exports.uploadPdfBuffer = async (buffer) => {
   const fileName = `${process.env.MY_CLOUDCUBE}/public/NICE-InContact-Recommendation-Report-${Date.now()}.pdf`;
   const filePath = `${process.env.CLOUDCUBE_URL}/${fileName}`;
 
-  const uploadParams = { Bucket: 'cloud-cube', Key: fileName, Body: buffer };
+  const uploadParams = { Bucket: 'cloud-cube', Key: fileName, Body: pdfBuffer };
 
   s3.upload(uploadParams, (err) => {
     if (err) {
